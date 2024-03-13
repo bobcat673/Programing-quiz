8cac2f70938f9c2035635a361df4bb81f9de76c9
Aidan Portwood <aidanportwood0@gmail.com>
2024-03-13 13:45:26 -0700

    make sure the repo is up to date

--- js/script.js
+++ js/script.js
@@ -1,21 +1,36 @@
 
 
 window.onload = function() {
-  document.getElementById("submit").onclick = function(event) {
-    event.preventDefault()
-    let name = document.getElementById("Name").value;
-    let num = document.getElementById("Num").value;
+  document.getElementById("clear").onclick = function(event) {
+    event.preventDefault();
+    let link = document.getElementById("link");
     let outH = document.getElementById("outputH");
     let outP = document.getElementById("outputP");
-    let animal = document.getElementById("animal").value;
+    outH.innerText = "";
+    outP.innerText = "";
+    link.innerText = "";
+    link.setAttribute("href", "#");
+  };
+  document.getElementById("submit").onclick = function(event) {
+    event.preventDefault();
+    let link = document.getElementById("link");
+    let name = document.getElementById("Name").value;
+    let outH = document.getElementById("outputH");
+    let outP = document.getElementById("outputP");
     let project = document.querySelector('input[name="favProject"]:checked').value;
-    outH.innerText = "Hello " + name + " here are your results"
+    outH.innerText = "Hello " + name + " here are your results";
     if (project=== "AI") {
-      outP.innerText = "You should learn python"
+      outP.innerText = "You should learn Python";
+      link.setAttribute("href" , "https://www.python.org/about/gettingstarted/");
+      link.innerText = "You can learn about it here!";
     } else if (project === "Web") {
-      outP.innerText = "You should learn javascript"
+      outP.innerText = "You should learn Javascript";
+      link.setAttribute("href" , "https://www.w3schools.com/js/");
+      link.innerText = "You can learn about it here!";
     } else if (project === "Style") {
-      outP.innerText = "You should learn css"
+      outP.innerText = "You should learn CSS ";
+      link.setAttribute("href" , "https://www.w3schools.com/css/");
+      link.innerText = "You can learn about it here!";
     } else {
       outP.innerText = "You should select a project"
     }
